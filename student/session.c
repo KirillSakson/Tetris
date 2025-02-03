7c2d30d3074f18a314332d234d9edea53374df02
piber06 <136062343+piber06@users.noreply.github.com>
2025-02-03 23:06:26 +0100

    Update session.c

--- student/session.c
+++ student/session.c
@@ -1,11 +1,11 @@
 #include "session.h"
 
 void restart_session_game_state(Session *session){
-    init_game_state(&session->current_game_state);
+    init_game_state(&session->current_game_state);    // initialization of a game state
 }
 
 void init_session(Session *session){
-    session->best_score = 0;
-    restart_session_game_state(session);
+    session->best_score = 0;    // setting best score to 0
+    restart_session_game_state(session);    // restarting the state game of the session
 }
 
