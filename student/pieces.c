de3ceb9ba382d66cd528ba36e35e5f26fcbb212f
piber06 <136062343+piber06@users.noreply.github.com>
2025-02-04 20:33:03 +0100

    Update pieces.c

--- student/pieces.c
+++ student/pieces.c
@@ -31,118 +31,129 @@ void print_piece(Piece p)
     }
 }
 
-void rotate_counter_clockwise(Piece *p)
-{
-    for (int i = 0; i < PIECE_SIZE; ++i)
-    {
-        for (int j = 0; j < PIECE_SIZE; ++j)
-        {
-            p->board[i][j] = p->board[-j][i];
-            // rotation--> by changing coordinates and inversing the first one
-        }
-    }
-}
-
 void rotate_clockwise(Piece *p)
 {
-    for (int i = 0; i < 3; ++i)
-    {
-        rotate_counter_clockwise(p);
-        // rotating 3 times counter clockwise == 1 time clockwise; 2 times counter clockwise == 2 times clockwise, and so on
+    char temp[PIECE_SIZE][PIECE_SIZE];  // temporary matrix
+
+    // rotation --> 90º clockwise
+    for (int i = 0; i < p->rows; ++i){
+        for (int j = 0; j < p->cols; ++j){
+            temp[j][p->rows-1-i]=p->board[i][j];
+        }
+    }
+
+    // used parts of matrix are being copied
+    for (int i = 0; i < p->cols; ++i) { // cols = rows of new
+        for (int j = 0; j <p->rows; ++j) {  // rows = cols of new
+            p->board[i][j] = temp[i][j];
+        }
+    }
+
+    // changing rows for cols and cols for rows
+    int temp_size = p->rows;
+    p->rows = p->cols;
+    p->cols = temp_size;
+}
+
+void rotate_counter_clockwise(Piece *p)
+{
+    for (int i = 0; i < 3; ++i) {
+        rotate_clockwise(p);
+        // rotating 3 times clockwise == 1 time counter clockwise;
     }
 }
 
 Piece make_O()
 {
-    Piece piece = {
-        .name = 'O',
-        .rows = 2,
-        .cols = 2,
-        .board = {
-            {'*', '*', '.', '.'},
-            {'*', '*', '.', '.'},
-            {'.', '.', '.', '.'},
-            {'.', '.', '.', '.'}}};
+    Piece piece;
+    init_piece(&piece);
+    piece.name = 'O';
+    piece.rows = piece.cols = 2;
+    piece.board[0][0] = '#'; piece.board[0][1] = '#';
+    piece.board[1][0] = '#'; piece.board[1][1] = '#';
     return piece;
 }
 
 Piece make_I()
 {
-    Piece piece = {
-        .name = 'I',
-        .rows = 4,
-        .cols = 1,
-        .board = {
-            {'*', '.', '.', '.'},
-            {'*', '.', '.', '.'},
-            {'*', '.', '.', '.'},
-            {'*', '.', '.', '.'}}};
+    Piece piece;
+    init_piece(&piece);
+    piece.name = 'I';
+    piece.rows = 4;
+    piece.cols = 1;
+    piece.board[0][0] = '#';
+    piece.board[1][0] = '#';
+    piece.board[2][0] = '#';
+    piece.board[3][0] = '#';
     return piece;
 }
 
 Piece make_S()
 {
-    Piece piece = {
-        .name = 'S',
-        .rows = 2,
-        .cols = 3,
-        .board = {
-            {'.', '*', '*', '.'},
-            {'*', '*', '.', '.'},
-            {'.', '.', '.', '.'},
-            {'.', '.', '.', '.'}}};
+    Piece piece;
+    init_piece(&piece);
+    piece.name = 'S';
+    piece.rows = 2;
+    piece.cols = 3;
+    piece.board[0][1] = '#';
+    piece.board[0][2] = '#';
+    piece.board[1][0] = '#';
+    piece.board[1][1] = '#';
+    return piece;
 }
 
 Piece make_Z()
 {
-    Piece piece = {
-        .name = 'Z',
-        .rows = 2,
-        .cols = 3,
-        .board = {
-            {'*', '*', '.', '.'},
-            {'.', '*', '*', '.'},
-            {'.', '.', '.', '.'},
-            {'.', '.', '.', '.'}}};
+    Piece piece;
+    init_piece(&piece);
+    piece.name = 'Z';
+    piece.rows = 2;
+    piece.cols = 3;
+    piece.board[0][0] = '#';
+    piece.board[0][1] = '#';
+    piece.board[1][1] = '#';
+    piece.board[1][2] = '#';
     return piece;
 }
 
 Piece make_L()
 {
-    Piece piece = {
-        .name = 'L',
-        .rows = 3,
-        .cols = 2,
-        .board = {
-            {'*', '.', '.', '.'},
-            {'*', '.', '.', '.'},
-            {'*', '*', '.', '.'},
-            {'.', '.', '.', '.'}}};
+    Piece piece;
+    init_piece(&piece);
+    piece.name = 'L';
+    piece.rows = 3;
+    piece.cols = 2;
+    piece.board[0][0] = '#';
+    piece.board[1][0] = '#';
+    piece.board[2][0] = '#';
+    piece.board[2][1] = '#';
+    return piece;
 }
 
 Piece make_J()
 {
-    Piece piece = {
-        .name = 'J',
-        .rows = 3,
-        .cols = 2,
-        .board = {
-            {'.', '*', '.', '.'},
-            {'.', '*', '.', '.'},
-            {'*', '*', '.', '.'},
-            {'.', '.', '.', '.'}}};
+    Piece piece;
+    init_piece(&piece);
+    piece.name = 'J';
+    piece.rows = 3;
+    piece.cols = 2;
+    piece.board[0][1] = '#';
+    piece.board[1][1] = '#';
+    piece.board[2][0] = '#';
+    piece.board[2][1] = '#';
+    return piece;
 }
 
 Piece make_T()
 {
-    Piece piece = {
-        .name = 'T',
-        .rows = 2,
-        .cols = 3,
-        .board = {
-            {'*', '*', '*', '.'},
-            {'.', '*', '.', '.'},
-            {'.', '.', '.', '.'},
-            {'.', '.', '.', '.'}}};
+    Piece piece;
+    init_piece(&piece);
+    piece.name = 'T';
+    piece.rows = 2;
+    piece.cols = 3;
+    piece.board[0][0] = '#';
+    piece.board[0][1] = '#';
+    piece.board[0][2] = '#';
+    piece.board[1][1] = '#';
     return piece;
 }
