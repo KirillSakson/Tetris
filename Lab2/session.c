9507e79e3064e5cbe54ddbeeb550599f10a20b02
KirillSakson <144152917+KirillSakson@users.noreply.github.com>
2025-02-18 19:44:24 +0100

    session.c commented

--- Lab2/session.c
+++ Lab2/session.c
@@ -1,10 +1,12 @@
 #include "session.h"
 
 void restart_session_game_state(Session *session){
+    // restarts current game state of the session
     restart_game_state(&(session->current_game_state));
 }
 
 void init_session(Session *session){
+    // initialises a new session
     session->best_score = 0;    // setting best score to 0
     init_game_state(&(session->current_game_state));
 }
